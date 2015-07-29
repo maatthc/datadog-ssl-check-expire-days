4916bc41435d1de3276544d94aebe7fbbbc25e09
Alexandre Silva <asilva@ea.com>
2015-07-29 17:01:57 +1000

    Implemented Frequency control, Datadog Events and Custom checks with WARNING and ERROR levels.

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-This is a cool way to check the expire date for all your SSL sites. Now instead of adding calendar events, you can just do monitoring with alerting
+# Monitoring SSL Certificates Expiring date with DataDog
+
+This generates DataDog (http://datadoghq.com) Events and Custom checks with WARNING and ERROR levels, Instead of ploting metrics to a graphic and alerting on that metric.
+It also defines how often this test should run to avoid overhead.
+
+Screenshot: http://i62.tinypic.com/33tqqua.png
+
+## How to use
+ - Copy the file ssl_check_expire_days.py to the folder /etc/dd-agent/checks.d
+ - Copy the file ssl_check_expire_days.yaml to the folder /etc/dd-agent/config.d
+ - Edit the file /etc/dd-agent/config.d/ssl_check_expire_days.yaml and include your websites address as well as your thresholds for critical and warning alerts
+ - Restart the Datadog Agent 
+
+## How to test the alert
+After copying the files and setting the configuration, you can:
+
+### Run it as a regular Python Script
+ATTENTION - The script will fail when it looks for the api_key but it can help in case you are modifying the code.
+
+From the folder /opt/datadog-agent/agent, run
+```shell
+PYTHONPATH=. /opt/datadog-agent/embedded/bin/python /etc/dd-agent/checks.d/ssl_check_expire_days.py
+```
+### Run as a DataDog Test
+It will show you some information about the Event contents and so on: 
+```shell 
+sudo -u dd-agent dd-agent check ssl_check_expire_days
+```
+
+## Credits
+Based on https://github.com/dobber/datadog-ssl-check-expire-days:
+
+"This is a cool way to check the expire date for all your SSL sites. Now instead of adding calendar events, you can just do monitoring with alerting
 
 The original source is https://workshop.avatarnewyork.com/project/datadog-ssl-expires-check/ and I just changed the way it works.
 On the original source, it checked for certificates in the file system. On my version, the script checks for web site certificates.
@@ -7,4 +39,4 @@ For information on how to set it up, go to the original author's web site. Only 
 
 All credits to the original author.
 
-I love datadog :)
+I love datadog :) "
--- ssl_check_expire_days.py
+++ ssl_check_expire_days.py
@@ -5,11 +5,21 @@ import time
 import datetime
 import subprocess
 import sys
+from hashlib import md5
+
 from checks import AgentCheck
+
+# Default Alert Text
+Alert_name = "SSL Certificate is about the expire"
+# Use this to instruct your team on how to renew the Certificate
+Check_Message = "Please consult here (xx) to see how to renew it. "
+
  
 class SSLCheckExpireDays(AgentCheck):
     def check(self, instance):
-        metric = "ssl.expire_in_days"
+        Critical = int(instance.get('thresholds').get('critical'))
+        Warning = int(instance.get('thresholds').get('warning'))
+
         site = instance['site']
         p = subprocess.Popen("echo | openssl s_client -connect " + site + ":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -f 2 -d\= | xargs -0 -I arg date -d arg \"+%s\"",stdout=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
@@ -18,9 +28,44 @@ class SSLCheckExpireDays(AgentCheck):
             d0 = int(time.time())
             d1 = int(output)
             delta = d1 - d0
-            days= delta/24/60/60 # convert the timestamp to days
+            days_to_expire= delta/24/60/60 # convert the timestamp to days
             tag="site:" + site # generate the tags
-#            print "metric: " + str(metric) + ", tag: " + str(tag) + ", days: " + str(days)
-            self.gauge(metric, int(days), tags=[tag])
+            print "\nDays to expire: %s - To be Critical should be less than:  %s - To be Warnings should be less than: %s ." % (days_to_expire,Critical,Warning)
+            print "tag: " + str(tag)
+            #self.gauge(metric, int(days), tags=[tag])
+            if days_to_expire < Critical:
+                self.event_expire("error", instance, days_to_expire)
+                self.service_check(Alert_name,2,None,None,None,None,Check_Message)
+            elif days_to_expire < Warning:
+                self.service_check(Alert_name,1,None,None,None,None,Check_Message)
+                self.event_expire("warning", instance, days_to_expire)
+            else:
+                # All good
+                self.service_check(Alert_name,0,None,None,None,None,Check_Message)
+
         else:
-            self.gauge(metric, -1, tags=[site])
+            #self.gauge(metric, -1, tags=[site])
+            self.service_check("Failed to check SSL Certificate for the site %s" %site,1)
+
+    def event_expire (self, level, instance, days_to_expire):
+         #self.log.info("%s %s - %s" %(Alert_name,days_to_expire,level))
+         # Use a hash of the URL as an aggregation key
+         aggregation_key = md5(instance['site']).hexdigest()
+         self.event({
+            'timestamp': int(time.time()),
+            'event_type': 'SSLCertificateExpire',
+            'msg_title': Alert_name,
+            'msg_text': '%s : %s .It is %s days to expire!' %(Alert_name,instance['site'],days_to_expire),
+            'aggregation_key': aggregation_key,
+            'alert_type': level
+        })
+
+if __name__ == '__main__':
+    check, instances = SSLCheckExpireDays.from_yaml('/etc/dd-agent/conf.d/ssl_check_expire_days.yaml')
+    for instance in instances:
+        print "\nRunning the check against cert: %s ." % (instance['site'])
+        check.check(instance)
+        if check.has_events():
+            print 'Events: %s' % (check.get_events())
+        print 'Metrics: %s' % (check.get_metrics())
+
